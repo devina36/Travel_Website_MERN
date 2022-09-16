1179dee33dbfdd86ec8612b96502ea0d93410780
Devina Amalia <devina.amalia36@gmail.com>
2022-09-16 23:45:29 +0700

    star component

--- src/parts/Testimonial.js
+++ src/parts/Testimonial.js
@@ -2,33 +2,50 @@ import React from 'react'
 
 import testiFrame from 'assets/images/testimonial-landingpages-frame.jpg'
 
-import star from 'assets/images/icons/star.svg'
+import Star from 'elements/Star'
 import Button from "elements/Button";
 
-export default function Testimonial(props) {
+export default function Testimonial({data}) {
   return (
     <section className='container' style={{ marginTop: 100 }}>
-        <div className="row justify-content-between align-items-center">
-            <div className="col-5">
-                <div style={{ width: 365, height: 501}} className="position-relative">
-                    <img src={props.data.imageUrl} alt="hero-img" className="img-fluid position-absolute" style={{ zIndex: 1, margin:'40px 0 0 40px' }} />
-                    <img src={testiFrame} alt="heroframe-img" className="img-fluid position-absolute" style={{ margin: '0 -15px -15px 0'  }}/>
+        <div className="row align-items-center">
+            <div className="col-auto" style={{ paddingRight: 45 }}>
+                <div 
+                style={{margin:'40px 0 0 40px'}} 
+                className="position-relative testi_img">
+                    <img 
+                    src={data.imageUrl} alt="hero-img" 
+                    className="img-fluid position-absolute" 
+                    style={{ zIndex: 1}} 
+                    />
+                    <img 
+                    src={testiFrame} alt="heroframe-img" 
+                    className="img-fluid position-absolute" 
+                    style={{ margin: '-40px 0 0 -40px'  }}
+                    />
                 </div>
             </div>
-            <div className="col-7 pl-0">
-                <h4 className='mb-5'>{props.data.name}</h4>
-                <div className="d-flex">
-                    <img src={star} alt="" />
-                    <img src={star} alt="" />
-                    <img src={star} alt="" />
-                    <img src={star} alt="" />
-                    <img src={star} alt="" />
-                </div>
-                <h2 className='font-weight-light' style={{ lineHeight: 1.5, width: '90%' }}>{props.data.content}</h2>
+            <div className="col-7">
+                <h4 className='mb-5'>{data.name}</h4>
+                <Star 
+                value={data.rate} 
+                width={35} 
+                spacing={4} 
+                height={35}
+                ></Star>
+                <h2 
+                className='font-weight-light' 
+                style={{ lineHeight: 1.5, width: '92%' }}
+                >
+                    {data.content}
+                </h2>
                 <span className='text-gray-500'>
-                    {props.data.familyName}, {props.data.familyOccupation}
+                    {data.familyName}, {data.familyOccupation}
                 </span>
-                <Button className='btn px-5 btn-shadow d-block' style={{ marginTop: 60 }} 
+                <Button 
+                className='btn px-5 btn-shadow d-block' 
+                style={{ marginTop: 50 }} 
+                href={`/testimonial/${data._id}`}
                 hasShadow 
                 isPrimary
                 >
