133eacfa59f192129b5a627ddcc72b2672aeb5dc
Devina Amalia <devina.amalia36@gmail.com>
2022-09-21 05:07:20 +0700

    f

--- src/parts/BrandIcon.js
+++ src/parts/BrandIcon.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function BrandIcon() {
   return (
-    <Button className='brand-icon' href="" type='Link'>Stay
+    <Button className='brand-icon' href="/" type='Link'>Stay
         <span className='text-gray-900'>cation.</span>
     </Button>
   )
